a112c7bcdfb131313e4f8812a0f752061a1c22fe
s22043-pj-Szymon-Stefanski <73427627+s22043-pj-Szymon-Stefanski@users.noreply.github.com>
2022-07-03 18:43:00 +0200

    Add files via upload

--- jaz-22043-nbp/src/main/java/com/example/jaz22043nbp/Repository/ZlotoRepository.java
+++ jaz-22043-nbp/src/main/java/com/example/jaz22043nbp/Repository/ZlotoRepository.java
@@ -2,6 +2,8 @@ package com.example.jaz22043nbp.Repository;
 
 import com.example.jaz22043nbp.Entity.ZlotoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ZlotoRepository extends JpaRepository<ZlotoModel, Long> {
 }
